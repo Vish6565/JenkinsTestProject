f283745920bf047f8d5ee26a3dbaf3e837406957
APPLE <apple@APPLEs-MacBook-Air.local>
2019-07-07 10:09:23 +0530

    First commit

---
+++ src/com/Rest/PracticeTest.java
@@ -0,0 +1,14 @@
+package com.Rest;
+
+public class PracticeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String test = "Vishal";
+
+		System.out.println("You Are Awesome " + test);
+
+	}
+
+}
